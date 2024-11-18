bf44a8f6db7fa760a6c64c9e9bea2473ab33fe50
Artag/Chris <scristxyz@gmail.com>
2024-11-18 09:37:59 -0500

    act de rutas

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,8 +1,9 @@
 
+"use client"
 import Navbar from "@/components/navbar/Navbar";
 import React, { Suspense } from "react";
 import dynamic from 'next/dynamic';
-
+import { useSession } from "next-auth/react"
 
 // Componente cliente para el Sidebar
 const ClientSidebar = dynamic(() => import("@/components/sidebar/ClientSidebar"), {
@@ -15,12 +16,19 @@ export default function PublicLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { data: session } = useSession()
   return (
     <>
+    {session?(
+      <>
       <Navbar />
       <Suspense fallback={<div>aqui</div>}>
       <ClientSidebar>{children}</ClientSidebar>
       </Suspense>
+       </>
+      ):
+       <div>no session</div>}
+      
     </>
   );
 }
